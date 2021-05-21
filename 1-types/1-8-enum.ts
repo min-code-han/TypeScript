61e065aa35ee20c49097ed22d4bee60aa0658358
alda <alda@hanmin-aui-MacBookPro.local>
2021-05-21 18:30:31 +0900

    ENUM

---
+++ 1-types/1-8-enum.ts
@@ -0,0 +1,20 @@
+{
+  //Enum 
+  // 독립적인 상수들..??
+  // 가능한 사용하지 않는것이 좋아요
+  
+  //JavaScript
+  const MAX_NUM = 6;
+  const MAX_STUDENTS_PER_CLASS = 10;
+  const MONDAY = 0;
+  const TUESDAY = 1;
+  const WEDNESDAY = 2;
+  const DAYS_ENUM = Object.freeze({"MONDAY":0, "TUESDAY":1, "WEDNESDAY":2})
+  const dayOfToday = DAYS_ENUM .MONDAY
+
+
+  //TypeScript
+
+
+
+} 
