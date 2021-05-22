fc6e4071dfd1dbaf532e91ffe8be6bd278e15fb0
mina <yegrinaaa@gmail.com>
2021-05-22 20:09:57 +0900

    Caculate 해답

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,25 +1,43 @@
 /**
  * Let's make a calculator 🧮
  */
-function calculate(name: string, a: number, b: number) {
-  if (name === 'add') {
-    return a + b;
-  }
-  if (name === 'substract') {
-    return a + b;
-  }
-  if (name === 'multiply') {
-    return a * b;
-  }
-  if (name === 'divide') {
-    return a / b;
-  }
-  if (name === 'remainder') {
-    return a % b;
-  }
-}
+// function calculate(name: string, a: number, b: number) {
+//   if (name === 'add') {
+//     return a + b;
+//   }
+//   if (name === 'substract') {
+//     return a + b;
+//   }
+//   if (name === 'multiply') {
+//     return a * b;
+//   }
+//   if (name === 'divide') {
+//     return a / b;
+//   }
+//   if (name === 'remainder') {
+//     return a % b;
+//   }
+// }
 console.log(calculate('add', 1, 3)); // 4
 console.log(calculate('substract', 3, 1)); // 2
 console.log(calculate('multiply', 4, 2)); // 8
 console.log(calculate('divide', 4, 2)); // 2
 console.log(calculate('remainder', 5, 2)); // 1
+
+type Command = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+function calculate(command: Command, a: number, b: number): number {
+  switch (command) {
+    case 'add':
+      return a + b;
+    case 'substract':
+      return a - b;
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      return a / b;
+    case 'remainder':
+      return a % b;
+    default:
+      throw new Error('Unknown error');
+  }
+}
