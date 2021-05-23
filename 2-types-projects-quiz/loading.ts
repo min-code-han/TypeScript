5cf0417788a81a0fa20fbb7a736b9e2f9682240d
mina <yegrinaaa@gmail.com>
2021-05-23 23:42:30 +0900

    loading solution

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -19,19 +19,35 @@
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
-  function printLoginState(resourceLoadState: ResourceLoadState) {
-    if (resourceLoadState.state === 'loading') {
-      console.log('👀 loading...');
-    }
-    if (resourceLoadState.state === 'success') {
-      console.log('😃 loaded');
-    }
-    if (resourceLoadState.state === 'fail') {
-      console.log('😱 no network');
-    }
-  }
+  // function printLoginState(resourceLoadState: ResourceLoadState) {
+  //   if (resourceLoadState.state === 'loading') {
+  //     console.log('👀 loading...');
+  //   }
+  //   if (resourceLoadState.state === 'success') {
+  //     console.log('😃 loaded');
+  //   }
+  //   if (resourceLoadState.state === 'fail') {
+  //     console.log('😱 no network');
+  //   }
+  // }
 
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+
+  function printLoginState(state: ResourceLoadState) {
+    switch (state.state) {
+      case 'loading':
+        console.log('👀 loading...');
+        break;
+      case 'success':
+        console.log(`😃 ${state.response.body}`);
+        break;
+      case 'fail':
+        console.log(`😱 ${state.reason}`);
+        break;
+      default:
+        throw new Error(`unknon state :${state}`);
+    }
+  }
 }
