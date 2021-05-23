1645ca92c8c7697b56f3f1c2952bdfafe47eef05
mina <yegrinaaa@gmail.com>
2021-05-23 17:31:25 +0900

    좌표게임 완성

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,6 +1,35 @@
 /**
  * Let's make a game 🕹
  */
+
+// type LOCATION = 'up' | 'down' | 'left' | 'right';
+// function move(location: LOCATION) {
+//   switch (location) {
+//     case 'up':
+//       return;
+//   }
+// }
+
+const position = { x: 0, y: 0 };
+function move(direction: 'up' | 'down' | 'left' | 'right') {
+  switch (direction) {
+    case 'up':
+      position.y += 1;
+      break;
+    case 'down':
+      position.y -= 1;
+      break;
+    case 'left':
+      position.x -= 1;
+      break;
+    case 'right':
+      position.x += 1;
+      break;
+    default:
+      throw new Error(`unknown direction ${direction}`);
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
