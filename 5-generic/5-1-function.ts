723c36714e49b4d0bc11e85b0909a2f4bd3c56fa
mina <yegrinaaa@gmail.com>
2021-07-04 18:35:32 +0900

    generic 사용하기

--- 5-generic/5-1-function.ts
+++ 5-generic/5-1-function.ts
@@ -44,6 +44,19 @@
   const result = checkNotNullAnyBad(123);
   // 이 함수의 경우는 타입이 보장이 되지 않아요..! result가 any가 되어 버려요.=> 타입이 안전하지가 않아요.
 
+  //이럴때 사용 하는 것이 바로 GENERIC 🎉!!
+  // GENERIC은 어떤 타입 이던지 받을수 있고, 코딩을 할때, 사용할때 타입이 정해지기 때문에 타입을 보장 맏을수 있어ㅏ요.
+
+  function checkNotNull<GENERIC>(arg: GENERIC | null): GENERIC {
+    if (arg == null) {
+      throw new Error('not valid number!');
+    }
+    return arg;
+  }
+  const number = checkNotNull(123); // 이 시점에 타입이 정해져요.
+  const bool: boolean = checkNotNull(true); // 이렇게 하면 타입이 더 정확 하겠죠??
+  //어떤 인자를 전달 하던지, null이 아닌겅우에만 똑같은 타입을 전달 하는 함수에요.
+
 
 
 
