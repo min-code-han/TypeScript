f4c9ba4a1d84dd539b3d02a1ac9449c57460cff4
mina <yegrinaaa@gmail.com>
2021-07-04 18:29:19 +0900

    타입체크 하는 함수의 나쁜 예제 두번쨰

--- 5-generic/5-1-function.ts
+++ 5-generic/5-1-function.ts
@@ -25,12 +25,27 @@
 // }
 
 {
-  function checkNotNull(arg: number | null): number {
+  function checkNotNullBad(arg: number | null): number {
     if(arg == null ){
       throw new Error('not valid number');
     }
     return arg;
   }
-  checkNotNull(321);
-  checkNotNull(null); // 그런데 지금 이 함수는 number만 확인 할 수 있어요.
+  checkNotNullBad(321);
+  checkNotNullBad(null); // 그런데 지금 이 함수는 number만 확인 할 수 있어요.
+
+
+  function checkNotNullAnyBad (arg: any | null): any{
+    if(arg == null ){
+      throw new Error('not valid number');
+    }
+    return arg; 
+  } 
+  const result = checkNotNullAnyBad(123);
+  // 이 함수의 경우는 타입이 보장이 되지 않아요..! result가 any가 되어 버려요.=> 타입이 안전하지가 않아요.
+
+
+
+
+
 }
